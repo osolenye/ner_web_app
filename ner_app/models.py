a1c5c45d3d94829bf91bd60b0437c88b5cdb37e9
osolenye <raiimkulov.bekzat@gmail.com>
2025-03-18 22:52:22 +0600

    fucked up form saving

--- ner_app/models.py
+++ ner_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class QueryHistory(models.Model):
+    text = models.TextField()  # Текст запроса
+    entities = models.JSONField(default=list)  # Извлеченные сущности (в формате JSON)
+    created_at = models.DateTimeField(auto_now_add=True)  # Время создания записи
+
+    def __str__(self):
+        return f"Query: {self.text[:50]}..."
--- ner_app/views.py
+++ ner_app/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import spacy
+from .models import QueryHistory  # Импортируем модель
 
 # Загрузка модели spaCy
 nlp = spacy.load("en_core_web_sm")  # Для русского языка: "ru_core_news_sm"
 
+
 def index(request):
-    return render(request, 'index.html')
+    # Получаем историю запросов
+    history = QueryHistory.objects.all().order_by('-created_at')[:10]  # Последние 10 запросов
+    return render(request, 'index.html', {'history': history})
+
 
 def extract_entities(request):
     if request.method == 'POST':
@@ -16,6 +21,12 @@ def extract_entities(request):
         # Извлечение именованных сущностей
         entities = [{"label": ent.label_, "text": ent.text} for ent in doc.ents]
 
-        return render(request, 'index.html', {'entities': entities})
+        # Сохранение запроса в базу данных
+        QueryHistory.objects.create(text=text, entities=entities)
+
+        # Получаем историю запросов
+        history = QueryHistory.objects.all().order_by('-created_at')[:10]
+
+        return render(request, 'index.html', {'entities': entities, 'history': history})
 
     return render(request, 'index.html')
